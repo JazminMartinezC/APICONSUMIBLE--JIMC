d70a0c16e2131e60160f671d3d143ed21a2ee78c
JazminMartinezC <21160705@itoaxaca.edu.mx>
2024-12-01 21:36:29 -0600

    Subir

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -66,7 +66,7 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class UserService {
   private apiUrl = 'https://api.escuelajs.co/api/v1/users';
-  private newsApiUrl = 'https://newsapi.org/v2/top-headlines?q=trump&apiKey=b88050a0cf334cb5b4b827d63dff6f8d';
+  private newsApiUrl = 'https://newsapi.org/v2/top-headlines?q=trump';
   private apiKey = 'b88050a0cf334cb5b4b827d63dff6f8d';
   //ivate apiKey = 'eb98591c9c824958aa14d5b885b78ed9';
   private loggedInUser: any = null;
